ece1bb2bddbb3432d963aea4b75b337b251693e7
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-01-13 20:55:19 +0200

    chore: Add dependencies

--- Package.swift
+++ Package.swift
@@ -5,11 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "termfarm",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.7.0")
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "termfarm"
+            name: "termfarm",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ],
         ),
     ]
 )
